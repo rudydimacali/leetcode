4e76df3453453d4d74a88d640153949cc846dd4c
Rudy <rudydimacali@gmail.com>
2019-03-07 10:02:55 -0800

    Complete firstMissingPositive.js.

---
+++ Hard/firstMissingPositive.js
@@ -0,0 +1,45 @@
+/*
+
+https://leetcode.com/problems/first-missing-positive/
+
+Given an unsorted integer array, find the smallest missing positive integer.
+
+Example 1:
+
+Input: [1,2,0]
+Output: 3
+Example 2:
+
+Input: [3,4,-1,1]
+Output: 2
+Example 3:
+
+Input: [7,8,9,11,12]
+Output: 1
+Note:
+
+Your algorithm should run in O(n) time and uses constant extra space.
+
+*/
+
+const assert = require('assert');
+
+const firstMissingPositive = (nums) => {
+  let temp;
+  let i = 0;
+  while (i < nums.length) {
+    if (nums[i] > 0 && nums[i] <= nums.length && nums[nums[i] - 1] !== nums[i]) {
+      temp = nums[nums[i] - 1];
+      nums[nums[i] - 1] = nums[i];
+      nums[i] = temp;
+    } else {
+      i += 1;
+    }
+  }
+  for (let j = 0; j < nums.length; j += 1) {
+    if (nums[j] !== j + 1) return j + 1;
+  }
+  return nums.length + 1;
+};
+
+assert.equal(firstMissingPositive([-100, 0, 100]), 1);
