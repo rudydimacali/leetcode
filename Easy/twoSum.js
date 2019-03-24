ddbe6f2026b40faaf420626d5973f5519eec0cce
Rudy <rudydimacali@gmail.com>
2019-03-24 16:04:12 -0700

    Refactor twoSum to O(n) runtime.

--- Easy/twoSum.js
+++ Easy/twoSum.js
@@ -17,15 +17,23 @@ Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
 */
 
+const assert = require('assert');
+
 const twoSum = (nums, target) => {
   const result = [];
-  nums.forEach((number, index, nums) => {
-    for (let i = index + 1; i < nums.length; i += 1) {
-      if (number + nums[i] === target) {
-        result.push(index, i);
-        return;
-      }
+  const differences = {};
+  for (let i = 0; i < nums.length; i += 1) {
+    differences[target - nums[i]] = i;
+  }
+  console.log(differences);
+  for (let i = 0; i < nums.length; i += 1) {
+    if (differences[nums[i]] !== undefined && differences[nums[i]] !== i) {
+      result.push(i, differences[nums[i]]);
+      differences[nums[i]] = undefined;
+      differences[target - nums[i]] = undefined;
     }
-  });
+  }
   return result;
 };
+
+assert.deepEqual(twoSum([3, 2, 4], 6), [1, 2]);
