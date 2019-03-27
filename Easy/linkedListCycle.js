8239cdf1ea050ca93e64b6d2ccba80b2164f5df6
Rudy <rudydimacali@gmail.com>
2019-03-27 14:20:02 -0700

    Bug fixes for return values for linkedListCycle.

--- Easy/linkedListCycle.js
+++ Easy/linkedListCycle.js
@@ -19,12 +19,12 @@ Explanation: There is a cycle in the linked list, where tail connects to the sec
 */
 
 const hasCycle = (head) => {
-  if (!head || !head.next) return head;
+  if (!head || !head.next) return false;
   let slow = head;
-  let fast = head;;
-  while (slow.next) {
+  let fast = head;
+  while (slow.next && fast && fast.next) {
     slow = slow.next;
-    if (fast.next) fast = fast.next.next;
+    fast = fast.next.next;
     if (slow === fast) return true;
   }
   return false;
